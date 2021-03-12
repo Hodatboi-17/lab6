5fa1ae749aca5e2815b0d107925f4a5383843d8f
Hodatboi-17 <d_dauitov@kbtu.kz>
2021-03-13 00:37:00 +0600

    git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {NoturlComponent} from './noturl/noturl.component';
+import {AlbumsComponent} from './albums/albums.component';
+import {AlbumsDetailsComponent} from './albums-details/albums-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'albums', component: AlbumsComponent},
+  {path: 'albums/:id', component: AlbumsDetailsComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: NoturlComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NoturlComponent } from './noturl/noturl.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AlbumsDetailsComponent } from './albums-details/albums-details.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NoturlComponent,
+    AlbumsComponent,
+    AlbumsDetailsComponent,
+    HttpClientModule,
   ],
   imports: [
     BrowserModule,
